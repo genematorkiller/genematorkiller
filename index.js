9956bb19d983a73b38cac4b80c56f0a54462afcd
genematorkiller <gnematorkiller@gmail.com>
2021-03-13 16:46:45 +0500

    genematorkiller commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const util = require('util');
+const fs = require('fs');
+
+
+const filePathName = path.resolve('./genematorkiller.txt')
+const writeFile = util.promisify(fs.writeFile)
+const readFile = util.promisify(fs.readFile)
+
+
+async function writeFn(path) {
+
+   let contentArr = []
+   
+
+   try {
+     contentArr = JSON.parse(await readFile(path, 'utf8'))
+   } catch (err) {
+     console.error(err)
+   }
+
+   let newContent = [...contentArr, '.']
+
+   writeFile(path, JSON.stringify(newContent))
+}
+
+writeFn(filePathName)
---
+++ script.sh
@@ -0,0 +1,7 @@
+node index.js;
+git init
+git add .
+git commit -m "genematorkiller commit"
+git branch -M main
+git remote add origin https://github.com/genematorkiller/genematorkiller.git
+git push -u origin main
