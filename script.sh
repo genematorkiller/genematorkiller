a4cfc248bc05ba1f8be0c968d6ca49a31f1e5de3
genematorkiller <gnematorkiller@gmail.com>
2021-03-13 16:53:33 +0500

    genematorkiller commit

--- script.sh
+++ script.sh
@@ -1,7 +1,4 @@
 node index.js;
-git init
 git add .
 git commit -m "genematorkiller commit"
-git branch -M main
-git remote add origin https://github.com/genematorkiller/genematorkiller.git
 git push -u origin main
