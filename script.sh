c6c441d79899d335c3232b16840d0dd882842eea
genematorkiller <gnematorkiller@gmail.com>
2021-03-13 17:12:19 +0500

    genematorkiller commit (counter)

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ node index.js;
 git add .
 for (( counter=15; counter>0; counter-- ))
 do
-git commit -m "genematorkiller commit counter"
+git commit -m "genematorkiller commit (counter)"
 node index.js;
 git add .
 done
